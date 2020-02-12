754f7661a00747a71ae4f209b548ef386c6c15a1
Virginia Ladd <virginialadd2@gmail.com>
2020-02-12 09:25:01 -0700

    Create the app constructor and fetch the url data for reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from '../Form/Form.js'
+import Form from '../Form/Form.js';
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+  componentDidMount = () => {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(reservations =>  this.setState({ reservations }))
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +23,7 @@ class App extends Component {
           <Form />
         </div>
         <div className='resy-container'>
-
+          <ReservationContainer reservations={this.state.reservations}/>
         </div>
       </div>
     )
