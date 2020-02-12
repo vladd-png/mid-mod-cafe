1a56ae81cad729bdae08a0c1d13eccd691cc1ad3
Virginia Ladd <virginialadd2@gmail.com>
2020-02-12 09:41:07 -0700

    Update card with props

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './ReservationCard.css';
 
 const ReservationCard = props => {
   return (
-    <article>
-      <h1>{props.name}</h1>
-      <h2>{props.date}</h2>
-      <h2>{props.time}</h2>
-      <h2>Number of Guests: {props.number}</h2>
-      <button type='text'>Cancel </button>
+    <article className='reso-card'>
+      <h1 id='card-name'>{props.name}</h1>
+      <h2 className='card-details'>{props.date}</h2>
+      <h2 className='card-details'>{props.time}</h2>
+      <h2 className='card-details'>Number of Guests: {props.number}</h2>
+      <button className='submit-btn' type='text'>Cancel </button>
     </article>
   )
 }
