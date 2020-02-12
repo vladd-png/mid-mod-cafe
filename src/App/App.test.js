f68e70072021cba958efc4202e90cc1dba8d2552
Virginia Ladd <virginialadd2@gmail.com>
2020-02-12 10:32:00 -0700

    Create and pass a fetch test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -11,13 +11,25 @@ it('renders without crashing', () => {
 
 describe('App', () => {
   let wrapper;
+  let mockResponse = { name: 'Charlie Day', date: '04/01', time: '7pm', number: '8' }
 
   beforeEach(() => {
     wrapper=shallow(<App />);
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      })
+    })
   })
 
   it('should be an instance of App', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should update update reservation state when componentDidMount is called', () => {
+    wrapper.instance().componentDidMount();
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/reservations')
   })
 
 })
