c778dc0810b526dae4c63be2930ca629e5be65a0
Virginia Ladd <virginialadd2@gmail.com>
2020-02-12 09:40:42 -0700

    Import card into container

--- src/ReservationContainer/ReservationContainer.js
+++ src/ReservationContainer/ReservationContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReservationCard from '../ReservationCard/ReservationCard.js';
+import './ReservationContainer.css';
 
 const ReservationContainer = props => {
   let allReservations = props.reservations.map(reso => <ReservationCard key={reso.id} name={reso.name} date={reso.date} number={reso.number} time={reso.time} />)
