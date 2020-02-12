8ec00c9d990cd98f92c11a9196ddc82e8a102811
Virginia Ladd <virginialadd2@gmail.com>
2020-02-12 10:58:48 -0700

    Match styling better

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -6,7 +6,7 @@ const ReservationCard = props => {
     <article className='reso-card'>
       <h1 id='card-name'>{props.name}</h1>
       <h2 className='card-details'>{props.date}</h2>
-      <h2 className='card-details'>{props.time}</h2>
+      <h2 className='card-details'>{props.time} pm</h2>
       <h2 className='card-details'>Number of Guests: {props.number}</h2>
       <button className='submit-btn' type='text' onClick={() => props.cancelReservation(props.id)}>Cancel </button>
     </article>
